9fa26e3dd199333585513201106b55baed440032
deyafa <deyafa.cloud@gmail.com>
2021-08-18 17:58:04 +0200

    hh

---
+++ src/scripts/script.js
@@ -0,0 +1,13 @@
+// function hamada() {
+//     alert("hhhhhhhhhhhhhhhhhhhhh")
+    
+// }
+var showdialog = document.querySelector('#show');
+function hamada() {
+    showdialog.classList.add("dialog--show");
+}
+
+function hide() {
+    showdialog.classList.remove("dialog--show");
+}
+
